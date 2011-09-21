df18136467dee74e9ebb26ff9c84639e712f6dcd
Bryan Lor <lorbs28@gmail.com>
2011-09-20 23:26:33 -0500

    Assets precompiled

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,11 +1,11 @@
 Juicee::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb
-
+  #config.assets.enabled = true;
 # Code is not reloaded between requests
   config.cache_classes = true
 
   # Full error reports are disabled and caching is turned on
-  config.consider_all_requests_local       = true
+  config.consider_all_requests_local       = false
   config.action_controller.perform_caching = false
 
   # Disable Rails's static asset server (Apache or nginx will already do this)
@@ -15,7 +15,7 @@ Juicee::Application.configure do
   config.assets.compress = true
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
-  config.assets.compile = false
+  config.assets.compile = true
 
   # Generate digests for assets URLs
   config.assets.digest = true
@@ -43,7 +43,7 @@ Juicee::Application.configure do
   # config.action_controller.asset_host = "http://assets.example.com"
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
-  config.assets.precompile += ['default.css']
+  #config.assets.precompile += ['default.css']
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
