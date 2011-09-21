f1b28810a2d1c5702166465adbc7e6d6c5b704d7
Bryan Lor <lorbs28@gmail.com>
2011-09-20 20:44:21 -0500

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Juicee::Application.config.secret_token = 'b0e1d559e777784046f63f511087b07df7a62e4be4fa3ade3ae403972ad79b74aff5896d0a7a98075fd282da87902ff1c48ef2c2638a8cc96706fbb7d883a99e'
